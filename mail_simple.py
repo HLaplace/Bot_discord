835641e050292b81197b5aeafc0f43713c516fbe
Hugola33290 <61471263+Hugola33290@users.noreply.github.com>
2021-05-05 15:00:53 +0100

    Add files via upload

---
+++ mail_simple.py
@@ -0,0 +1,43 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+import time
+import os 
+import msvcrt 
+from email.mime.multipart import MIMEMultipart
+
+def send_mail(destinataire,subject,message_text):
+
+	mail_sender = 'coucou@gmail.com'
+	mail_sender_password = 'password'
+
+	message = MIMEMultipart()   
+	message['To'] = destinataire
+	message['Subject'] = subject
+	msg = message_text
+	message.attach(MIMEText(msg.encode('utf-8'), 'plain', 'utf-8'))
+ 
+	serveur = smtplib.SMTP('smtp.gmail.com', 587)   
+	serveur.starttls()   
+	serveur.login(mail_sender, mail_sender_password)   
+	texte= message.as_string().encode('utf-8')   
+	serveur.sendmail('nom expediteur', message['To'], texte)   
+	serveur.quit()   
+	print("mail send succesfull")
+
+####################################################################
+
+while 1:
+
+	mail = input("Recever ? ")
+	texte = input("Body ? ")
+	sujet = input("Subject ? ")
+	send_mail(mail,sujet,texte)
+
+	print("")
+	print("press esc to escape or press any key to contiue : ")
+	print("")
+	char = char = msvcrt.getch()
+	if char == b'\x1b':
+	    os.system('cls')
+	    exit()
+
